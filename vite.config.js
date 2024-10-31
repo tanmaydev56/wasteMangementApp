e69fcf61e6c369ab33b1fa149f155c803943e971
Tanmay <tanmaysharma763@gmail.com>
2024-10-31 18:30:09 +0530

    changges in vercel.json and package.json3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  build: {
+    outDir: 'dist', // Ensures the output folder is named "dist"
+  },
+  define: {
+    'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+  },
 });
